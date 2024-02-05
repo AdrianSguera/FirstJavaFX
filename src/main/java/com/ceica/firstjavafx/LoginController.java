d5f2d172faf66d18d584a0e3ee609f62a8797b06
Adrian_Sguera <adrianfsguera@gmail.com>
2024-02-05 20:45:34 +0100

    Initial commit

---
+++ src/main/java/com/ceica/firstjavafx/LoginController.java
@@ -0,0 +1,2 @@
+package com.ceica.firstjavafx;public class LoginController {
+}
---
+++ src/main/java/com/ceica/firstjavafx/UserController.java
@@ -0,0 +1,13 @@
+package com.ceica.firstjavafx;
+
+public class UserController {
+    private String datos;
+
+    public String getDatos() {
+        return datos;
+    }
+
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
+}
